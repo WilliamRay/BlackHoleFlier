582b00f039b68db679211a78220f5652da26fb2e
William Ray <williamray@williams-mbp.home>
2014-08-01 21:44:02 +0100

    Initial Commit

---
+++ BlackHoleFlyer/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  BlackHoleFlyer
+//
+//  Created by William Ray on 01/08/2014.
+//  Copyright (c) 2014 Roger Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlackHoleFlyerTests/BlackHoleFlyerTests.m
@@ -0,0 +1,34 @@
+//
+//  BlackHoleFlyerTests.m
+//  BlackHoleFlyerTests
+//
+//  Created by William Ray on 01/08/2014.
+//  Copyright (c) 2014 Roger Ray. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlackHoleFlyerTests : XCTestCase
+
+@end
+
+@implementation BlackHoleFlyerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
